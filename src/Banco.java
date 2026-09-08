63de78a2d4d5694406c73845040ac89f44ab36ab
Shikamaru777 <eliucoelhorodrigues8@gmail.com>
2026-09-08 07:53:56 -0300

    Primeiro commit

---
+++ src/Banco.java
@@ -0,0 +1,33 @@
+public class Banco {
+
+    //ATRIBUTOS
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private Float saldo;
+    private boolean status;
+
+    public Banco () {
+        saldo = 0f;
+        status = false;
+    }
+
+
+    //METODOS
+    public void abrirConta() {
+
+    }
+    public void fechaConta(){
+
+    }
+    public void depositar(){
+
+    }
+    public void sacar() {
+
+    }
+    public void pagarMensal() {
+
+    }
+
+}
